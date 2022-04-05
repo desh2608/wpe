601eb4a0a902ad80e750c1888b3121e6b0b880c1
Desh Raj <r.desh26@gmail.com>
2022-04-05 10:30:25 -0400

    working implementation with cupy

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# wpe
-Nara-WPE in CuPy for GPU inference
+# Nara-WPE in CuPy
+
+This package is modified from the core parts of [nara_wpe](https://github.com/fgnt/nara_wpe) 
+and modifies it to use [CuPy](https://github.com/cupy/cupy) for accelerated GPU-based inference.
+
+At the moment, it is meant to be used with the [GSS](https://github.com/desh2608/gss) toolkit.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="wpe",
+    version="0.1.0",
+    description="Simplified Nara WPE in CuPy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/desh2608/wpe",
+    author="Desh Raj",
+    author_email="r.desh26@gmail.com",
+    keywords="speech enhancement gss",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),  # Required
+)
